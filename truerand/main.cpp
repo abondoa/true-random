1f44c3a87e050f45105cd9fc0a5f2eb3163b38f1
abondoa <abondoa@gmail.com>
2013-03-12 15:15:08 +0100

    cli

--- truerand/main.cpp
+++ truerand/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <boost\program_options.hpp>
 #include "../Random/RandomNumberGeneratorFactory.h"
 #include "../Random/Generator.h"
 
@@ -6,16 +7,57 @@
 
 using namespace std;
 using namespace Random;
+namespace po = boost::program_options;
+
+template <class T> void generate();
 
 int main(int argc, char** argv)
 {
-	Generator<unsigned int>* gen = RandomNumberGeneratorFactory::GetInstance()->GetGenerator<unsigned int>();
+	int bytes;
+	po::options_description desc("Allowed options");
+	desc.add_options()
+		("help,h", "Produce help message")
+		("bytes,b", po::value<int>(&bytes)->default_value(4), "Set number of bytes pr generation: 1, 2, 4, 8")
+	;
+	po::variables_map vm;
+	po::store(po::parse_command_line(argc, argv, desc), vm);
+	po::notify(vm);
+	
+	if (vm.count("help")) {
+		cout << desc << "\n";
+		return 1;
+	}
+	switch(bytes)
+	{
+	case 1:
+		generate<unsigned char>();
+		break;
+	case 2:
+		generate<unsigned short>();
+		break;
+	case 4:
+		generate<unsigned int>();
+		break;
+	case 8:
+		generate<unsigned long long>();
+		break;
+	default:
+		cout << desc << "\n";
+		return 1;
+	}
+	
+
+	return 0;
+}
+
+template <class T> void generate()
+{
+	Generator<T>* gen = RandomNumberGeneratorFactory::GetInstance()->GetGenerator<T>();
 
 	for ever
 	{
-		cout << gen->Generate() << endl;
+		cout << (unsigned long long)gen->Generate() << endl;
 	}
 
 	delete gen;
-	return 0;
 }
